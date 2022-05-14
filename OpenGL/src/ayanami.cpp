92afbd1d92d4541edccf801410cce5e6c5200f2c
AMEXif <amex.if.email@seznam.cz>
2022-05-14 13:26:37 +0200

    Rei Ayanami

--- OpenGL/src/ayanami.cpp
+++ OpenGL/src/ayanami.cpp
@@ -18,7 +18,6 @@ int main(void)
         glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(window);
 
     if (glewInit() != GLEW_OK)
@@ -26,18 +25,32 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    /* Vertex buffer */
+    float pos[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+
+    /* Bind and Enable buffer! */
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glEnableVertexAttribArray(0);
+
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), pos, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    /* Main loop */
     while (!glfwWindowShouldClose(window))
     {
         /* Rendering */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        /* Swap front and back buffers */
+        /* Swap back and front buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
